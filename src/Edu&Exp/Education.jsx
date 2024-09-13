3d7a37c40eb6e718e77828fdddc27c1cd16d9af5
iftikhrlutfur <iftikherlutfur@gmail.com>
2024-09-13 10:25:08 +0600

    Added the routes of Education

---
+++ src/Edu&Exp/Education.jsx
@@ -0,0 +1,12 @@
+
+const Education = () => {
+    return (
+        <div>
+           <h1>Diploma In Computer Science & Technology </h1>
+           <p>Habiganj Polytechnic Institute</p>
+           <p>2021-22 to Present</p> 
+        </div>
+    );
+};
+
+export default Education;
